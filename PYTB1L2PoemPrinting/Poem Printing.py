9f6612d41c05585a05fd58b6a5eb90ed7770dfea
Jahvairo <33427@ma-web.nl>
2021-10-07 18:05:17 +0200

    Trello aftekenen

---
+++ PYTB1L2PoemPrinting/Poem Printing.py
@@ -0,0 +1,6 @@
+if True:
+ vers1 = "Ik sta bekend als de gene die gaat als jahva, mensen denken vaak hij spoort niet, hijs skaffa. Maar niks is minder waar, Amsterdam de plek want ik woon daar. ben imiddels 15 jaar heb er nog een aantal te gaan."
+ vers2 = "hopelijk kan ik binnenkort als programmeur op mn eigen benen staan. Soms is het lastig waar ik mee kan leven daarom zal ik alles geven. ookal heb ik een gigantische hekel aan fouten in me codes die uitsteken en de hele applicatie breken."
+ vers3 = "Speel allerlei spellen waarover ik kan vertellen ik zal er eentje uitspellen genaamd T-L-O-P. als je het niet kent is dat ook, oké waar ik ook ga neem ik mijn oortjes mee. JV zonder muziek is geen goed idee"
+
+print (vers1,vers2, vers3) 
