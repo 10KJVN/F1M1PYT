294828e4c2c68e2c58f6518d2ee0a6867aee74c1
Jahvairo <33427@ma-web.nl>
2021-11-04 13:13:21 +0100

    2nd commit

--- PythonAchievements/PYTB1L4TheFinalCountdown.py
+++ PythonAchievements/PYTB1L4TheFinalCountdown.py
@@ -1,9 +1,5 @@
-fruits = ["apple", "banana", "cherry"]
-for BABA in fruits:
-  if BABA == "banana":
-    print(BABA)
-    i = 1
-    while i < 11:
-        print(i)
-        i -= 1
+i = 1000
+while i > 0 :
+  print(i)
+  i -= 1
         
